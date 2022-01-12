8a6c321699f6c4e0e310d44c5eeccc6f982bfcdd
Cahier <31394482+CahierX@users.noreply.github.com>
2022-01-12 15:39:14 +0800

    init

--- ws.js
+++ ws.js
@@ -1,24 +1,24 @@
 const WebSocket = require('ws');
 
-class ServiceWebSocket {
+class Ws {
   constructor(url) {
     this.webSocket = null;
     this.url = url;
     this.connectTimer = null;
   }
 
-  initServiceWebSocket() {
+  initWebSocket() {
     return new Promise((resolve) => {
       this.webSocket = new WebSocket(this.url);
       if (this.webSocket) {
-        this.webSocket.addEventListener('error', this.serviceWebSocketOnerror.bind(this));
-        this.webSocket.addEventListener('close', this.serviceWebSocketOnClose.bind(this));
+        this.webSocket.addEventListener('error', this.webSocketOnerror.bind(this));
+        this.webSocket.addEventListener('close', this.webSocketOnClose.bind(this));
       }
       resolve(true);
     });
   }
 
-  serviceWebSocketSend(data) {
+  webSocketSend(data) {
     if (this.webSocket !== null && this.webSocket.readyState === 1) {
       data.params = { ...data.params, client: global.$mac ? 'mac' : 'windows' };
       const sendData = { ...data, timestamp: String(new Date().getTime()) };
@@ -29,7 +29,7 @@ class ServiceWebSocket {
     }
   }
 
-  serviceWebSocketOnopen() {
+  webSocketOnopen() {
     return new Promise((resolve) => {
       this.connectTimer = null;
       this.connectTimer = setInterval(() => this.checkConnect(), 60000);
@@ -48,22 +48,22 @@ class ServiceWebSocket {
   }
 
   async checkConnect() {
-    this.serviceWebSocketSend({ eventName: 'ping', msgId: Math.random() });
+    this.webSocketSend({ eventName: 'ping', msgId: Math.random() });
   };
 
-  serviceWebSocketOnerror(error) {
+  webSocketOnerror(error) {
     if (this.webSocket && this.webSocket.readyState) {
       if (this.webSocket.readyState === 3) {
         this.webSocket = null;
         console.log(`${process.platform} webSocket err: `, error);
-        this.initServiceWebSocket();
+        this.initWebSocket();
       }
     }
   }
 
-  serviceWebSocketOnClose() {
+  webSocketOnClose() {
     console.log(`${process.platform} webSocket close reconnect`);
-    this.initServiceWebSocket();
+    this.initWebSocket();
   }
 
   destroyWebSocket() {
@@ -79,7 +79,7 @@ class ServiceWebSocket {
     });
   }
 
-  serviceWebSocketOnmessage() {
+  webSocketOnmessage() {
     return new Promise((resolve) => {
       this.webSocket.onmessage = (e) => {
         if (this.webSocket) {
@@ -93,27 +93,27 @@ class ServiceWebSocket {
     });
   }
 
-  async getServiceWsData(eventName) {
-    const data = await this.serviceWebSocketOnmessage(eventName);
+  async getWsData(eventName) {
+    const data = await this.webSocketOnmessage(eventName);
     if (data) {
       if (data.eventName === eventName) {
         return data;
       } else {
-        return await this.getServiceWsData(eventName);
+        return await this.getWsData(eventName);
       }
     } else {
-      return await this.getServiceWsData(eventName);
+      return await this.getWsData(eventName);
     }
   };
 
   async sendWsData(eventName, params) {
     const msgId = Math.random();
-    this.serviceWebSocketSend({
+    this.webSocketSend({
       eventName,
       msgId,
       params
     });
-    return await this.getServiceWsData(eventName, msgId);
+    return await this.getWsData(eventName, msgId);
   };
 }
-module.exports = ServiceWebSocket;
+module.exports = Ws;
